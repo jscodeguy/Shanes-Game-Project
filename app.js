da5a653d26559600a80d19f17d25b9ee14a14740
jscodeguy <im.still.at.school99@gmail.com>
2022-02-22 21:35:37 -0500

    added new borders and got some of the health display and player collisions fixed

--- app.js
+++ app.js
@@ -39,12 +39,14 @@ class MugMen {
  ctx.drawImage(
     glovePic, 50, 50
 )}
-let player = new MugMen(10, 10, 'blue', 16, 16)
-let mug = new MugMen(600, 300, 'brown', 32, 64)
+let player = new MugMen(15, 15, 'blue', 15, 15)
+let mug = new MugMen(600, 300, 'brown', 30, 65)
 let glove = new MugMen(50, 50, 'transparent', 20,20 )
-let sprite = new MugMen(200, 200, 'limegreen', 32, 64)
-let borderLeft = new MugMen(4, 0, 'red', 4, 900)
-let borderRight = new MugMen(792, 0, 'red', 4, 900)
+let sprite = new MugMen(200, 200, 'limegreen', 30, 65)
+let borderLeft = new MugMen(0, 0, 'red', 5, 900)
+let borderRight = new MugMen(795, 0, 'red', 5, 900)
+let borderUp = new MugMen(0,0,'red',900,5)
+let borderDown = new MugMen(0,395,'red',900,5)
 document.addEventListener('DOMContentLoaded', function () {
     
     document.addEventListener('keydown', movementHandler)
@@ -55,9 +57,11 @@ document.addEventListener('DOMContentLoaded', function () {
 const gameLoop = () => {
     ctx.clearRect(0, 0, game.width, game.height)
     movement.textContent = player.x + ', ' + player.y
+    document.getElementById('health').textContent = playerHealth
     // player.render()
     if (player.alive) {
         player.render()
+        // document.getElementById('health').textContent = playerHealth
     }
     if (sprite.alive){
         detectSpriteHit()
@@ -83,7 +87,7 @@ const gameLoop = () => {
     }
     if (playerHealth === 0) {
         player.alive = false
-        document.getElementById('status').textContent = 'you die by a sprite!'
+        // document.getElementById('status').textContent = 'you die by a sprite!'
     }
     if (borderLeft.alive){
         borderLeft.render()
@@ -91,31 +95,42 @@ const gameLoop = () => {
     if (borderRight.alive){
         borderRight.render()
     }
+    if (borderUp.alive){
+        borderUp.render()
+    }
+    if (borderDown.alive){
+        borderDown.render()
+    }
 }
 const movementHandler = (e) => {
+    if (player.alive === true) {
     switch (e.keyCode) {
         case (87):
-            player.y -= 15
+            player.y -= 5
             break
         case (65):
-            player.x -= 15
+            player.x -= 5
             //sprite.x -= 10
             break
         case (83):
-            player.y += 15
+            player.y += 5
             break
         case (68):
-            player.x += 15
+            player.x += 5
             //sprite.x += 10
             break
+        }
+    }
+    else {
+        return false
     }
 }
 const spritePath = () => {
     if (spriteDirection === 'right'){
-        sprite.x += 4
+        sprite.x += 5
     }
     else if (spriteDirection === 'left') {
-        sprite.x -= 4
+        sprite.x -= 5
     }
 }
 const detectMugHit = () => {
@@ -154,21 +169,35 @@ const detectSpriteHit = () => {
             player.y -= 25
            // player.x -= 15
             playerHealth -= 10
-            document.getElementById('health').textContent = playerHealth
+            // document.getElementById('health').textContent = playerHealth
             //player.alive = false
             document.getElementById('status').textContent = 'You got punch by a sprite!'
         }
+        else if (playerHealth === 0) {
+            document.getElementById('status').textContent = 'you die by a sprite!'
+        }
 }
+//+ sprite.width
 const borderLeftHit = () => {
     if (sprite.x === borderLeft.x 
         && spriteDirection === 'left'){
             spriteDirection = 'right'
         }
+    if (player.x === borderLeft.x) {
+        player.x += 30
+        document.getElementById('status').textContent = 'The world punch you for trying to abandon mug'
+        playerHealth -=10
+    }
 }
 const borderRightHit = () => {
-    if (sprite.x + sprite.width === borderRight.x 
+    if (sprite.x  + sprite.width=== borderRight.x 
         && spriteDirection === 'right'){
             spriteDirection = 'left'
+        }
+    if (player.x + player.width === borderRight.x) {
+            player.x -= 30
+            document.getElementById('status').textContent = 'The world punch you for trying to abandon mug'
+            playerHealth-=10
         }
 }
 //console.log(isGloved)
